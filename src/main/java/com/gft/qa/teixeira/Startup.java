673fa371fe9355e538b5e31102afdfe7194e96b5
Tiago Teixeira <tteixeirati@gmail.com>
2025-01-22 15:07:28 -0300

    Including Browserstack yml

--- src/main/java/com/gft/qa/teixeira/Startup.java
+++ src/main/java/com/gft/qa/teixeira/Startup.java
@@ -31,11 +31,11 @@ public class Startup {
         options.setAppActivity("com.wdiodemoapp/.MainActivity");
         options.setAutomationName("UiAutomator2");
         options.setPlatformName("Android");
-        options.setPlatformVersion("15.0");
+        options.setPlatformVersion("12.0");
         options.setNewCommandTimeout(Duration.ofSeconds(60));
 
 //        options.setCapability("platformName", "Android"); // or "iOS"
-        options.setCapability("deviceName", "Google Pixel 9"); // Specify device
+        options.setCapability("deviceName", "Samsung Galaxy S22 Ultra"); // Specify device
         options.setCapability("app", "bs://0b3702a6ab69ccd0f740dd899056284cf3a876f7"); // Specify app ID in BrowserStack
         options.setCapability("browserstack.user", userName); // Set BrowserStack username
         options.setCapability("browserstack.key", accessKey); // Set BrowserStack access key
@@ -47,32 +47,6 @@ public class Startup {
         if (!driver.isAppInstalled("com.wdiodemoapp")) {
 //            driver.installApp(apkPath);
             driver.installApp("bs://0b3702a6ab69ccd0f740dd899056284cf3a876f7");
-        }
-        Thread.sleep(3000);
-        return driver;
-
-    }
-
-    public AndroidDriver startAppBrowserStack() throws Exception {
-
-        String currentDir = System.getProperty("user.dir");
-        String apkPath = currentDir + "\\android.wdio.native.app.v1.0.8.apk";
-        URL appiumServerUrl = new URL("http://172.30.208.1:4723/");
-
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("emulator-5554");
-        options.setAppPackage("com.wdiodemoapp");
-        options.setAppActivity("com.wdiodemoapp/.MainActivity");
-        options.setAutomationName("UiAutomator2");
-        options.setPlatformName("Android");
-        options.setPlatformVersion("15.0");
-        options.setNewCommandTimeout(Duration.ofSeconds(60));
-
-        driver = new AndroidDriver(appiumServerUrl, options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        if (!driver.isAppInstalled("com.wdiodemoapp")) {
-            driver.installApp(apkPath);
         }
         Thread.sleep(3000);
         return driver;
